4994301e8f4e1b211632299e0babdb214ccfb166
= <vigneshwaran.vicky1812@gmail.com>
2023-11-13 17:17:16 +0530

    Model added

--- app.py
+++ app.py
@@ -87,41 +87,48 @@ def signup():
 def predict_with_model():
     data = request.get_json()
     accx = data.get('accx')
-    accx = data.get('accx')
-    accx = data.get('accx')
+    accy = data.get('accy')
+    accz = data.get('accz')
     gyrx = data.get('gyrx')
     gyry = data.get('gyry')
     gyrz = data.get('gyrz')
     lat=data.get("lat")
     long=data.get("long")
+    age=data.get("age")
+    height=data.get("height")
+    weight=data.get("weight")
+    gender=data.get("gender")
+    g=0
+    if gender=='Male':
+        g=1
     # Load the model
-    model = tf.keras.models.load_model(model_path)
+    model1 = tf.keras.models.load_model("Acc.h5")
+    single_row_data_acc = np.array([[age,accx,accy, accz, height, weight, g]]).astype("float32")
+    single_row_data_acc = single_row_data_acc.reshape(1, 7, 1)
+    predictions = model1.predict(single_row_data_acc)
+    # Example with a threshold of 0.5
+    threshold = 0.5
+    binary_prediction1 = (predictions > threshold).astype(int)
+    model2 = tf.keras.models.load_model("Gyro.h5")
+    single_row_data_gyro = np.array([[age,gyrx,gyry, gyrz, height, weight, g]]).astype("float32")
+    single_row_data_gyro = single_row_data_gyro.reshape(1, 7, 1)
+    predictions = model2.predict(single_row_data_gyro)
+    # Example with a threshold of 0.5
+    threshold = 0.5
+    binary_prediction2 = (predictions > threshold).astype(int)
 
-    # Prepare input data for prediction
-    input_features = np.array([input_data['feature1'], input_data['feature2'], ...])  # Adjust based on your features
-
-    # Make predictions
-    predictions = model.predict(np.expand_dims(input_features, axis=0))
-
-    # You may need to post-process predictions based on your model and task
-    # For example, convert probabilities to class labels or regression values
-
-    # Prepare the result dictionary
+    # Print the binary prediction
+    fall="Fall"
+    if binary_prediction2[0][0]==0 and binary_prediction1[0][0]==0:
+        fall="Not Fall"
     result = {
-        'prediction': predictions.tolist(),
-        'class_labels': None,  # Add class labels if applicable
-        'additional_info': 'Your additional information here',
+        'prediction': fall,
+        "latitude":lat,
+        "longitude":long
+        
     }
 
     return result
-
-# Example usage:
-input_data = {
-    'feature1': 0.5,
-    'feature2': 1.2,
-    # Add other features as needed
-}
-
 
 @app.route('/api/login', methods=['POST'])
 def login():
